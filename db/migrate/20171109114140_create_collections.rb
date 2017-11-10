8e17da25df4e069f2b3e377c992d85d7bff61490
alexmorten <alex@Alexanders-MacBook-Pro.local>
2017-11-10 19:50:49 +0100

    made ids:uuid for collections, items and transactions

--- db/migrate/20171109114140_create_collections.rb
+++ db/migrate/20171109114140_create_collections.rb
@@ -1,6 +1,6 @@
 class CreateCollections < ActiveRecord::Migration[5.1]
   def change
-    create_table :collections do |t|
+    create_table :collections, id: :uuid do |t|
       t.string :name
       t.references :user, foreign_key: true
 
