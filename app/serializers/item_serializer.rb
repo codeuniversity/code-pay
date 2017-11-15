3db50c1f703234ede61e1f85a19d6215ac3aa6d6
alexmorten <alexandrius.martinus@gmx.de>
2017-11-15 21:12:43 +0100

    add amount_left to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :amount
+  attributes :id, :name, :price, :amount, :amount_left
   belongs_to :collection
   has_one :image
+  def amount_left
+  object.remaining_amount
+  end
 end
